14880ba46bf4ea72c9228e018c2e5991af4d5a4f
StanislavaTF <87148643+StanislavaTF@users.noreply.github.com>
2022-01-22 13:46:12 +0300

    Update LessonTwo.java

--- LessonTwo.java
+++ LessonTwo.java
@@ -6,35 +6,33 @@
 
 
 class LessonTwo {
-    public static void main (String [] args) {
-        within10and20 (7, 8);
-        within10and20 (-12, 6);
+    public static void main(String [] args) {
+        within10and20(7, 8);
+        within10and20(-12, 6);
 		
-        isPositiveOrNegative (-6);
-        isPositiveOrNegative (123);
+        isPositiveOrNegative(-6);
+        isPositiveOrNegative(123);
 		
-        System.out.println (isNegative (13));
-        System.out.println (isNegative (-33));
+        System.out.println(isNegative (13));
+        System.out.println(isNegative (-33));
 		
-        printString ("Eh bien, mon prince.", 9);
-	
-    
+        printString("Eh bien, mon prince.", 9);
     }
 	
-        static boolean within10and20(int a, int b) {
-            return a+b >= 10 && a+b <= 20;
+    static boolean within10and20(int a, int b) {
+        return a+b >= 10 && a+b <= 20;
         }
 	
-        static void isPositiveOrNegative(int x) {
-            System.out.println(x >= 0 ? "Positive" : "Negative");
+    static void isPositiveOrNegative(int x) {
+        System.out.println(x >= 0 ? "Positive" : "Negative");
         }
 	
-        static boolean isNegative(int c) {
-            return (c < 0 ? true : false);
+    static boolean isNegative(int c) {
+        return c < 0 ;
         }
 	
-        static void printString (String line, int amt) {
-            for (int i = 0; i < amt; i++)
-                System.out.println (line);
+    static void printString(String line, int amt) {
+        for (int i = 0; i < amt; i++)
+            System.out.println (line);
         }
 }
